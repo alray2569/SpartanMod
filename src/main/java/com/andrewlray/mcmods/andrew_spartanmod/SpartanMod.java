9eb331b90f9605304516471a09a89155101d94fb
Andrew Ray <alr2569@gmail.com>
2015-10-19 20:58:06 -0400

    Start Re-Commenting

--- src/main/java/com/andrewlray/mcmods/andrew_spartanmod/SpartanMod.java
+++ src/main/java/com/andrewlray/mcmods/andrew_spartanmod/SpartanMod.java
@@ -14,18 +14,49 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+/**
+ * The mod class for SpartanMod.
+ * 
+ * @author Andrew Ray
+ */
 @Mod(modid = Constants.MODID, name = Constants.MODNAME, version = "1.0.2")
 public class SpartanMod {
 
+	/**
+	 * The single static instance of the SpartanMod class.<br>
+	 * <br>
+	 * "There can only be one."
+	 */
 	@Mod.Instance(Constants.MODID)
 	public static SpartanMod instance;
-	
-	@SidedProxy(clientSide=Constants.PROXY_CLIENT, serverSide=Constants.PROXY_COMMON)
+
+	/**
+	 * The single static instance of the SpartanMod proxy. For calls made on the
+	 * client side, the value is the
+	 * {@linkplain com.andrewlray.mcmods.andrew_spartanmod.proxy.ClientProxy
+	 * ClientProxy}. For calls made on the server side, the value is the
+	 * {@linkplain com.andrewlray.mcmods.andrew_spartanmod.proxy.CommonProxy
+	 * CommonProxy}.<br>
+	 * <br>
+	 * "Like in quantum physics, the value is unknowable until it is observed.
+	 * When observed, its value changes based on its observer."
+	 */
+	@SidedProxy(clientSide = Constants.PROXY_CLIENT, serverSide = Constants.PROXY_COMMON)
 	public static CommonProxy proxy;
 
+	/**
+	 * The EventHandler that handles FMLPreInitializationEvents. It calls the
+	 * various preInit functions for the handlers, registers, and proxies in
+	 * SpartanMod.<br>
+	 * <br>
+	 * 
+	 * @param e
+	 *            The {@linkplain FMLPreInitializationEvent}.
+	 * @see com.andrewlray.mcmods.andrew_spartanmod.items.SMItems#preInit
+	 *      SMItems.preInit()
+	 */
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent e) {
-		// Bring out the penguins
 		SMItems.preInit();
 	}
 
@@ -40,7 +71,7 @@ public class SpartanMod {
 
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent e) {
-		
+
 	}
 
 }
